4a396246638fcfe9067b95a869003758782a7567
Jason <710410906@qq.com>
2024-03-11 13:13:51 +0800

    readme更新

--- 数据结构与算法/Code/src/com/dataStructure/queue/arrayQueue.java
+++ 数据结构与算法/Code/src/com/dataStructure/queue/arrayQueue.java
@@ -1,2 +1,64 @@
-package com.dataStructure.queue;public class arrayQueue {
+package com.dataStructure.queue;
+
+
+import java.util.Scanner;
+
+public class arrayQueue {
+    public static void main(String[] args) {
+        myQueue queue = new myQueue(3);
+        // 用于用户输入
+        char key;
+        Scanner scanner = new Scanner(System.in);
+        boolean loop = true;
+        while (loop) {
+            System.out.println("s(show): 显示队列");
+            System.out.println("a(add): 添加数据到队列");
+            System.out.println("g(get): 从队列取出数据");
+            System.out.println("h(head): 查看队列的数据");
+            System.out.println("e(exit): 退出程序队列");
+            key = scanner.next().charAt(0);
+            switch (key) {
+                case 's':
+                    queue.showQueue();
+                    break;
+                case 'a':
+                    System.out.print("向队列取存入1个数字：");
+                    int element = scanner.nextInt();
+                    try {
+                        queue.putElement(element);
+                    } catch (Exception e) {
+                        System.out.println();
+                        System.out.println(e.getMessage());
+                        System.out.println();
+                    }
+                    break;
+                case 'g':
+                    System.out.println("从队列取取出1个数字");
+                    try {
+                        System.out.printf("从队列中取到了数字%d\n", queue.getElement());
+                    } catch (Exception e) {
+                        System.out.println();
+                        System.out.println(e.getMessage());
+                        System.out.println();
+                    }
+                case 'h':
+                    System.out.println("查看队列头部数据");
+                    try {
+                        System.out.printf("当前队列的头部数据是%d", queue.showFirstElement());
+                    } catch (Exception e) {
+                        System.out.println();
+                        System.out.println(e.getMessage());
+                        System.out.println();
+                    }
+                    break;
+                case 'e':
+                    scanner.close();
+                    loop = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+        System.out.println("程序退出...");
+    }
 }
--- 数据结构与算法/Code/src/com/dataStructure/queue/myQueue.java
+++ 数据结构与算法/Code/src/com/dataStructure/queue/myQueue.java
@@ -1,2 +1,94 @@
-package com.dataStructure.queue;public class myQueue {
+package com.dataStructure.queue;
+
+/*
+* 队列实现
+*
+* 变量： 最大容量、头指针、尾指针、存储数组
+*
+* 方法： 判空、判满、存、取、显示
+*
+* */
+public class myQueue {
+    private int maxSize;
+    private int front;
+    private int rear;
+    private final int[] arr;
+
+    public myQueue(int queueSize) {
+        setFront(-1);
+        setRear(-1);
+        setMaxSize(queueSize);
+        arr = new int[maxSize];
+    }
+
+    public void showQueue() {
+        if (isQueueEmpty()) {
+            System.out.println("队列为空，暂无数据");
+            return;
+        }
+        System.out.println("队列信息如下：");
+        for (int i = front + 1; i < maxSize; i++) {
+            System.out.printf("%d\t%d\n", i, arr[i]);
+        }
+    }
+
+    public boolean isQueueFull() {
+        return rear == maxSize - 1;
+    }
+
+    public int showFirstElement() {
+        if (isQueueEmpty()) {
+            throw new RuntimeException("错误！无法显示头部数据，队列为空");
+        } else {
+            return arr[rear];
+        }
+    }
+
+    public boolean isQueueEmpty() {
+        return rear == front;
+    }
+
+    public void putElement(int newElement) {
+        // 向队列存放数据
+        if (isQueueFull()) {
+            throw new RuntimeException("error: the queue is full!");
+        } else {
+            rear++;
+            arr[rear] = newElement;
+        }
+    }
+
+    public int getElement() {
+        // 从队列取出数据
+        if (isQueueEmpty()) {
+            throw new RuntimeException("error: the queue is empty!");
+        } else {
+            front++;
+            return arr[front];
+        }
+    }
+
+    public int getFront() {
+        return front;
+    }
+
+    public void setFront(int front) {
+        this.front = front;
+    }
+
+    public int getRear() {
+        return rear;
+    }
+
+    public void setRear(int rear) {
+        this.rear = rear;
+    }
+
+    public int getMaxSize() {
+        return maxSize;
+    }
+
+    public void setMaxSize(int maxSize) {
+        this.maxSize = maxSize;
+    }
 }

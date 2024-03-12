a6bdd8e39f1aca791b9549f62532b4641d58f77b
Jason <710410906@qq.com>
2024-03-12 21:43:45 +0800

    20240312环形队列数组模拟

--- 数据结构与算法/Code/src/com/dataStructure/queue/arrayQueue.java
+++ 数据结构与算法/Code/src/com/dataStructure/queue/arrayQueue.java
@@ -1,15 +1,41 @@
 package com.dataStructure.queue;
 
-
 import java.util.Scanner;
 
 public class arrayQueue {
     public static void main(String[] args) {
-        myQueue queue = new myQueue(3);
         // 用于用户输入
         char key;
         Scanner scanner = new Scanner(System.in);
+        boolean programLoop = true;
+        while (programLoop) {
+            System.out.println("选择队列类型：");
+            System.out.println("  1.单向队列");
+            System.out.println("  2.环形队列");
+            System.out.println("退出程序（0）");
+            int type = scanner.nextInt();
+            switch (type) {
+                case 1:
+                    testQueue(scanner);
+                    break;
+                case 2:
+                    testCircleQueue(scanner);
+                    break;
+                case 0:
+                    scanner.close();
+                    programLoop = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+        System.out.println("程序退出...");
+    }
+
+    public static void testQueue(Scanner scanner) {
+        char key;
         boolean loop = true;
+        myQueue queue = new myQueue(3);
         while (loop) {
             System.out.println("s(show): 显示队列");
             System.out.println("a(add): 添加数据到队列");
@@ -41,6 +67,63 @@ public class arrayQueue {
                         System.out.println(e.getMessage());
                         System.out.println();
                     }
+                    break;
+                case 'h':
+                    System.out.println("查看队列头部数据");
+                    try {
+                        System.out.printf("当前队列的头部数据是%d\n", queue.showFirstElement());
+                    } catch (Exception e) {
+                        System.out.println();
+                        System.out.println(e.getMessage());
+                        System.out.println();
+                    }
+                    break;
+                case 'e':
+                    loop = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+        System.out.println("队列程序退出...");
+    }
+
+    public static void testCircleQueue(Scanner scanner) {
+        char key;
+        boolean loop = true;
+        myCircleQueue queue = new myCircleQueue(3);
+        while (loop) {
+            System.out.println("s(show): 显示队列");
+            System.out.println("a(add): 添加数据到队列");
+            System.out.println("g(get): 从队列取出数据");
+            System.out.println("h(head): 查看队列的数据");
+            System.out.println("e(exit): 退出程序队列");
+            key = scanner.next().charAt(0);
+            switch (key) {
+                case 's':
+                    queue.showQueue();
+                    break;
+                case 'a':
+                    System.out.print("向队列取存入1个数字：");
+                    int element = scanner.nextInt();
+                    try {
+                        queue.addElement(element);
+                    } catch (Exception e) {
+                        System.out.println();
+                        System.out.println(e.getMessage());
+                        System.out.println();
+                    }
+                    break;
+                case 'g':
+                    System.out.println("从队列取取出1个数字");
+                    try {
+                        System.out.printf("从队列中取到了数字%d\n", queue.getElement());
+                    } catch (Exception e) {
+                        System.out.println();
+                        System.out.println(e.getMessage());
+                        System.out.println();
+                    }
+                    break;
                 case 'h':
                     System.out.println("查看队列头部数据");
                     try {
@@ -52,13 +135,12 @@ public class arrayQueue {
                     }
                     break;
                 case 'e':
-                    scanner.close();
                     loop = false;
                     break;
                 default:
                     break;
             }
         }
-        System.out.println("程序退出...");
+        System.out.println("环形程序退出...");
     }
 }
---
+++ 数据结构与算法/Code/src/com/dataStructure/queue/myCircleQueue.java
@@ -0,0 +1,62 @@
+package com.dataStructure.queue;
+
+public class myCircleQueue {
+    private int maxSize;    // 数组的最大容量
+    private int front;  // 队列头
+    private int rear;   // 队列尾
+    private int[] arr;  // 储存数据
+    public myCircleQueue(int maxQueueSize) {
+        maxSize = maxQueueSize + 1;
+        arr = new int[maxSize];
+    }
+
+    public boolean isEmpty() {
+        return front == rear;
+    }
+
+    public boolean isFull() {
+        return (rear + 1) % maxSize == front;
+    }
+
+    public void addElement(int n) {
+        if (isFull()) {
+            System.out.println("队列已满，无法加入");
+            return;
+        }
+        arr[rear] = n;
+        // 环形队列要注意取模
+        rear = (rear + 1) % maxSize;
+    }
+
+    public int getElement() {
+        if (isEmpty()) {
+            throw new RuntimeException("队列为空，无法取出");
+        }
+        int value = arr[front];
+        // 环形队列要注意取模
+        front = (front + 1) % maxSize;
+        return value;
+    }
+
+    public int countElement() {
+        // 计算当前队列有多少个元素
+        return (maxSize + rear - front) % maxSize;
+    }
+
+    public void showQueue() {
+        if (isEmpty()) {
+            System.out.println("队列当前为空，无数据");
+            return;
+        }
+        for (int i = front; i < front + countElement(); i++) {
+            System.out.printf("arr[%d] = %d\n", (i - front) % maxSize, arr[i % maxSize]);
+        }
+    }
+
+    public int showFirstElement() {
+        if (isEmpty()) {
+            throw new RuntimeException("当前队列为空，无数据");
+        }
+        return arr[front];
+    }
+}

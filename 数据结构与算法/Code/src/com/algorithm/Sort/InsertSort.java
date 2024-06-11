8d3a4e19dab32b7ae50f930d119a395e4f632896
Jason <710410906@qq.com>
2024-06-11 19:21:10 +0800

    日期：20240611 内容：插入排序，React资料

--- 数据结构与算法/Code/src/com/algorithm/Sort/InsertSort.java
+++ 数据结构与算法/Code/src/com/algorithm/Sort/InsertSort.java
@@ -27,6 +27,23 @@ public class InsertSort {
         System.out.println("排序后：" + formatted2);
     }
 
+    /*
+    * [10, 8, 2, 6, 0] 原数组从i=1开始向后遍历，num[i]= 8, 有序列表从后往前遍历insertIndex = i - 1 = 0, insertNum = 8
+    * 循环交换位置，直至找到正确的位置，插入该值
+    * [10, 8, 2, 6, 0] -> [10, 10, 2, 6, 0]  i = 1 赋值原数组值后移一位，insertIndex前移一位，
+    * [10, 10, 2, 6, 0] -> [8, 10, 2, 6, 0]  i = 1 不满足insertIndex >= 0，insertIndex + 1 指向应放置insertNum位置
+    * [8, 10, 2, 6, 0] -> [8, 10, 10, 6, 0]  i = 2 赋值原数组值后移一位，insertIndex前移一位，
+    * [8, 10, 10, 6, 0] -> [8, 8, 10, 6, 0]  i = 2 赋值原数组值后移一位，insertIndex前移一位，
+    * [8, 8, 10, 6, 0] -> [2, 8, 10, 6, 0]   i = 2 不满足insertIndex >= 0，insertIndex + 1 指向应放置insertNum位置
+    * [2, 8, 10, 6, 0] -> [2, 8, 10, 10, 0]  i = 3 赋值原数组值后移一位，insertIndex前移一位，
+    * [2, 8, 10, 10, 0] -> [2, 8, 8, 10, 0]  i = 3 赋值原数组值后移一位，insertIndex前移一位，
+    * [2, 8, 8, 10, 0] -> [2, 6, 8, 10, 0]   i = 3 不满足insertNum < nums[insertIndex]，insertIndex + 1 指向应放置insertNum位置
+    * [2, 6, 8, 10, 0] -> [2, 6, 8, 10, 10]  i = 4 赋值原数组值后移一位，insertIndex前移一位，
+    * [2, 6, 8, 10, 10] -> [2, 6, 8, 8, 10]  i = 4 赋值原数组值后移一位，insertIndex前移一位，
+    * [2, 6, 8, 8, 10] -> [2, 6, 6, 8, 10]   i = 4 赋值原数组值后移一位，insertIndex前移一位，
+    * [2, 6, 6, 8, 10] -> [2, 2, 6, 8, 10]   i = 4 赋值原数组值后移一位，insertIndex前移一位，
+    * [2, 2, 6, 8, 10] -> [0, 2, 6, 8, 10]   i = 4 不满足insertIndex >= 0，insertIndex + 1 指向应放置insertNum位置
+    * */
     public static void insertSort(int[] nums, boolean needShowProgress) {
         for (int i = 1; i < nums.length; i++) { // {[有序列表],[无序列表]}，默认原数组第一个元素有序
             int insertIndex = i - 1;    // 从无序列表分界处前一个元素开始向前遍历
